cfa099c5cfabce8bcdda16ec2a709f555803a7fa
ANJAN PAUDEL <91400834+theAnjan@users.noreply.github.com>
2022-03-17 14:34:42 +0545

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,10 @@
+  <footer>
+      <div class="fixed-bottom" style="background-color:#5D6D7E ;">
+          <p style="text-align:center">Copyright &copy; Real state Sys</p>
+          
+      </div>
+     
+  </footer>
+
+</body>
+</html>
